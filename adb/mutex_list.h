810ccdc05ef2b9892d807b43eaeaa3150dd4493e
Josh Gao <jmgao@google.com>
2016-06-21 15:38:14 -0700

    DO NOT MERGE: adb: use asocket's close function when closing.
    close_all_sockets was assuming that all registered local sockets used
    local_socket_close as their close function. However, this is not true
    for JDWP sockets.
    
    Backport of 53eb31d8 and 9b587de.
    
    Bug: http://b/28347842
    Change-Id: I13ec6fd87a0691c72608baf6038adcc0f03dca9f
    (cherry picked from commit 5218ad36ad30d6a155d68fb779dca20ff220bf54)

--- adb/mutex_list.h
+++ adb/mutex_list.h
@@ -7,7 +7,6 @@
 #error ADB_MUTEX not defined when including this file
 #endif
 ADB_MUTEX(dns_lock)
-ADB_MUTEX(socket_list_lock)
 ADB_MUTEX(transport_lock)
 #if ADB_HOST
 ADB_MUTEX(local_transports_lock)
